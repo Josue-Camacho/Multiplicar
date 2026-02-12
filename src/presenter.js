b65f69a69ec0672c1013390fc879a50719b0029a
Josue-Camacho <josuecamacho582@gmail.com>
2026-02-11 21:19:49 -0400

    feat:add multiplicar nuevo boton

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,10 @@
-import sumar from "./sumador";
+import sumar, { multiplicar } from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const btnMultiplicar = document.querySelector("#btn-multiplicar");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +13,11 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+btnMultiplicar.addEventListener("click", () => {
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
